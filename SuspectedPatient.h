c1d80918cba1f825a014bce4e739223897d2696b
Muhammad Feroz <mferoz.bscs17seecs@seecs.edu.pk>
2020-04-07 13:08:03 +0000

    After Defining the classes for Route,Suspected Patient and Infected Patient

--- SuspectedPatient.h
+++ SuspectedPatient.h
@@ -10,9 +10,19 @@ class SuspectedPatient{
         int Spatient_id;
         string name;
         string phone;
+        bool quarantineFor14Days;
+
+
+
     public:
         int getSpatient_id();
         void setSpatient_id(int Spatient_id);
+        string getName();
+        void setName(string name);
+        string getPhone();
+        void setPhone(string phone);
+        bool getQuarantineFor14Days();
+        void setQuarantineFor14Days(bool quarantineFor14Days)
 };
 
 int SuspectedPatient::getSpatient_id(){
@@ -22,3 +32,21 @@ void SuspectedPatient::setSpatient_id(int Spatient_id){
 	this->Spatient_id = Spatient_id;
 }
 
+string SuspectedPatient::getName() {
+	return this->name;
+}
+void SuspectedPatient::setName(string name) {
+	this->name = name;
+}
+string SuspectedPatient::getPhone() {
+	return this->phone;
+}
+void SuspectedPatient::setPhone(string phone) {
+	this->phone = phone;
+}
+bool SuspectedPatient::getQuarantineFor14Days() {
+	return this->quarantineFor14Days;
+}
+void SuspectedPatient::setQuarantineFor14Days(bool quarantineFor14Days) {
+	this->quarantineFor14Days = quarantineFor14Days;
+}
